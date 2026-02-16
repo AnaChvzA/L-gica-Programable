a388873aa7ae3cbeca3526b54a0a0cff978ad9b9
Ana Cristina Chavez Acosta <a01742237@tec.mx>
2026-02-16 13:47:51 -0600

    Practica_1/Practica_1_Numeros_primos.v

--- Practica_1_Numeros_primos.v
+++ Practica_1_Numeros_primos.v
@@ -1,7 +1,15 @@
+// Ana Cristina Chávez Acosta - A01742237
+// Practica #1
+// Detector de números primos
+
+
+
 module Practica_1_Numeros_primos (
-	input [3:0] SW, 
-	output reg LED);
+	input [3:0] SW, //Inputs
+	output reg LED); //Output
 	
+	//Bloque de código que enciende el LED cuando los numeros son primos, en cualquier otro caso el LED se mantiene apagado
+	//Números primos que detecta 2,3,5,7,11,13 
 	always @(*)
 	 begin
 		case(SW)
@@ -11,7 +19,7 @@ module Practica_1_Numeros_primos (
 			4'b0111: LED = 1;
 			4'b1011: LED = 1;
 			4'b1101: LED = 1;
-			default: LED = 0;
+			default: LED = 0; //Cuando el numero no es primo, led está apagado
 		endcase
 	end
 	
